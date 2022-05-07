a9766e89a8e803552df477e77ab2617f883f6242
thiagoperest <thiago_teodoroperes07@hotmail.com>
2022-05-07 10:33:47 -0300

    Componente Card #02

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import React from "react";
 
 import Card from "./components/layout/Card";
@@ -8,23 +9,25 @@ import ComParametro from "./components/basicos/ComParametro";
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
   return (
-    <>
+    <div className="App">
       <h1>Fundamentos React!</h1>
 
-      <Card titulo="#03 - Desafio Aleatório">
-        <Aleatorio min={1} max={60} />
-      </Card>
+      <div className="Cards">
+        <Card titulo="#03 - Desafio Aleatório">
+          <Aleatorio min={1} max={60} />
+        </Card>
 
-      <Card titulo="#02 - Componente com Parâmetro">
-        <ComParametro
-          titulo="Situação do Aluno"
-          aluno="Thiago Peres"
-          nota={9.3}
-        />
-      </Card>
-      <Card titulo="#01 - Primeiro Componente">
-        <Primeiro />
-      </Card>
-    </>
+        <Card titulo="#02 - Componente com Parâmetro">
+          <ComParametro
+            titulo="Situação do Aluno"
+            aluno="Thiago Peres"
+            nota={9.3}
+          />
+        </Card>
+        <Card titulo="#01 - Primeiro Componente">
+          <Primeiro />
+        </Card>
+      </div>
+    </div>
   );
 };
